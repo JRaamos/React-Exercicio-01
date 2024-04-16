db11a8d83431dbbaa568937e296da66030256311
JRaamos <jhonyramos46@gmail.com>
2024-04-16 00:31:34 -0300

    feat: commit initial

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -14,11 +14,9 @@ export default function Book(props: BookProps) {
   return (
     <div className="book">
       <img src={ image } alt={ title } />
-      <h1>{ title }</h1>
-      <h3>{ author }</h3>
-      { showDetails && (
-        <p>{ description }</p>
-      ) }
+      <h1>{title}</h1>
+      <h3>{author}</h3>
+      {showDetails && <p>{description}</p>}
     </div>
   );
 }
